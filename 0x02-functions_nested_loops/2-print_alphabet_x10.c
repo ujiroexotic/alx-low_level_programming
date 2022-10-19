8d92ef930a7f630919436b956aa396b619f7d221
ujiroexotic <jiroeruteya@gmail.com>
2022-10-19 08:49:15 -0700

    Task 2. 10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,12 +7,12 @@
 void print_alphabet_x10(void)
 {
 	int count = 0;
-	char letter;
+	char n;
 
 	while (count++ <= 9)
 	{
-		for (letter = 'a'; letter <= 'z'; letter++)
-			_putchar(letter);
-		_putchar('/letter');
+		for (n = 'a'; n <= 'z'; n++)
+			_putchar(n);
+		_putchar('/n');
 	}
 }
