c3acbbac78bde29fd9ffcbf7031b3ebc6522f3fb
ujiroexotic <jiroeruteya@gmail.com>
2022-10-20 09:10:42 -0700

    Task 0. isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,17 +2,14 @@
 
 /**
  * _isupper - checks if a character is uppercase
- * @c: Variable test
+ * @c: c - Variable test
  * Return: Always 0
  */
 int _isupper(int c);
 {
-	if (c >= 'A' && c <= 'Z')
+	if (c >= 65 && c <= 90)
 	{
 		return (1);
 	}
-	else
-	{
 		return (0);
-	}
 }
