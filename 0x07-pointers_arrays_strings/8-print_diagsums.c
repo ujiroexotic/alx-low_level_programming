4ac85839994a5e325ed800074acce0239a02363a
ujiroexotic <jiroeruteya@gmail.com>
2022-11-08 14:33:29 -0800

    Task 8. Double pointer

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -11,16 +11,5 @@
  */
 void print_diagsums(int *a, int size)
 {
-	int i, j;
-	int d1 = 0, d2 = 0;
 
-	for (i = 0; i < size * size; i = i + (size + 1))
-	{
-		d1 = d1 + a[i];
-	}
-	for (j = size - 1; j < size * size - 1; j = j + (size - 1))
-	{
-		d2 = d2 + a[j];
-	}
-	printf("%d, %d\n", d1, d2);
 }
