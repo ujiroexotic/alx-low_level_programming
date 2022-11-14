a082ab0ceed215e35f1654d80eb44027fbac6240
ujiroexotic <jiroeruteya@gmail.com>
2022-11-14 13:06:22 -0800

    Task 4. A door is what

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -68,18 +68,19 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
 		return (NULL);
+
 	dog->name = malloc(sizeof(char) * (len1 + 1));
 	if (dog->name == NULL)
 	{
-		free(dog)
-		return (NULL)
+		free(dog);
+		return (NULL);
 	}
 	dog->owner = malloc(sizeof(char) * (len2 + 1));
 	if (dog->owner == NULL)
 	{
 		free(dog);
-		free(dog->name)
-			return (NULL)
+		free(dog->name);
+		return (NULL);
 	}
 
 	_strcpy(dog->name, name);
