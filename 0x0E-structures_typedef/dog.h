335e96c45ffbc58641773aa598b6a8f94604e68c
ujiroexotic <jiroeruteya@gmail.com>
2022-11-14 11:41:38 -0800

    Task 0. Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -6,10 +6,8 @@
  * @name: First member
  * @age: Second member
  * @owner: Third member
- *
  * Description: Longer description
  */
-
 struct dog
 {
 	char *name;
@@ -20,7 +18,6 @@ struct dog
 /**
  * dog_t - typedef for struct dog
  */
-
 typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
