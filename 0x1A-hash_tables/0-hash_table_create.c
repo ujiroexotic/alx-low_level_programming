8ac05f44c2d69e781c2d4cf24892233219501a4c
Ujiro Eruteya <ujiroeruteya@gmail.com>
2023-02-06 01:27:43 -0800

    0. >>> ht = {}

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -19,8 +19,8 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	ht->size = size;
 	ht->array = calloc((size_t)ht->size, sizeof(hash_node_t *));
-	if (ht->array == NUL)turn (ht);
+	if (ht->array == NULL)
+		return (NULL);
 
+	return (ht);
 }
-
-
